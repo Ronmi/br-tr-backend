b690af47af459da563595874e068c02fe0e5d8a9
Ronmi Ren <ronmi.ren@gmail.com>
2016-08-22 09:29:20 +0800

    add lock

--- mem_store.go
+++ mem_store.go
@@ -5,9 +5,12 @@ import "fmt"
 // MemStore stores project and branch info in memory
 type MemStore struct {
 	projects []Project
+	lock *sync.RWMutex
 }
 
 func (s *MemStore) List() (ret []Project, err error) {
+	s.lock.RLock()
+	defer s.lock.RUnlock()
 	ret = s.projects
 	if ret == nil {
 		ret = []Project{}
@@ -17,6 +20,8 @@ func (s *MemStore) List() (ret []Project, err error) {
 }
 
 func (s *MemStore) UpdateOwner(repo, branch, owner string) (err error) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
 	found := false
 	for i, p := range s.projects {
 		if p.Name != repo {
@@ -41,6 +46,8 @@ func (s *MemStore) UpdateOwner(repo, branch, owner string) (err error) {
 }
 
 func (s *MemStore) UpdateDesc(repo, branch, desc string) (err error) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
 	found := false
 	for i, p := range s.projects {
 		if p.Name != repo {
@@ -65,6 +72,8 @@ func (s *MemStore) UpdateDesc(repo, branch, desc string) (err error) {
 }
 
 func (s *MemStore) AddProjects(ps []Project) error {
+	s.lock.Lock()
+	defer s.lock.Unlock()
 	// create a repo_name => array index map
 	lookup := make(map[string]int)
 	for idx, p := range s.projects {
