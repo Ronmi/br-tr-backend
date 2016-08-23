1703dd316c64af8517a376e73d9bb156a19f0e51
Ronmi Ren <ronmi.ren@gmail.com>
2016-08-23 12:13:04 +0800

    implements the gitlab webhook

--- main.go
+++ main.go
@@ -1,11 +1,34 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"sync"
 
 	"github.com/Patrolavia/jsonapi"
+	gogitlab "github.com/plouc/go-gitlab-client"
 )
+
+type gitlabConf struct {
+	URL   string `json:"baseUrl"`
+	Path  string `json:"apiPath"`
+	Token string `json:"token"`
+}
+
+func loadGitlabConf(fn string) (ret gitlabConf) {
+	data, err := ioutil.ReadFile(fn)
+	if err != nil {
+		log.Fatalf("Cannot read config from %s: %s", fn, err)
+	}
+
+	if err = json.Unmarshal(data, &ret); err != nil {
+		log.Fatalf("Incorrect format of config file %s: %s", fn, err)
+	}
+
+	return
+}
 
 func main() {
 	store := &MemStore{[]Project{
@@ -32,13 +55,16 @@ func main() {
 			},
 		},
 	}, &sync.RWMutex{}}
+	glconf := loadGitlabConf("gitlab.json")
 
 	myapi := &api{store}
+	mywebhook := &webhook{gogitlab.NewGitlab(glconf.URL, glconf.Path, glconf.Token), store}
 
 	jsonapi.Register([]jsonapi.API{
 		jsonapi.API{"/api/list", myapi.list},
 		jsonapi.API{"/api/setOwner", myapi.setOwner},
 		jsonapi.API{"/api/setDesc", myapi.setDesc},
+		jsonapi.API{"/api/webhook", mywebhook.entry},
 	}, nil)
 
 	http.ListenAndServe(":8000", nil)
---
+++ webhook.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/Patrolavia/jsonapi"
+	gogitlab "github.com/plouc/go-gitlab-client"
+)
+
+type webhook struct {
+	client *gogitlab.Gitlab
+	store  DataStore
+}
+
+type webhookProject struct {
+	Path string `json:"path_with_namespace"`
+}
+type webhookAttr struct {
+	State           string         `json:"state"`
+	SourceProjectID int            `json:"source_project_id"`
+	TargetProjectID int            `json:"target_project_id"`
+	Source          webhookProject `json:"source,omitempty"`
+	Target          webhookProject `json:"target,omitempty"`
+}
+type webhookPayload struct {
+	Kind      string         `json:"object_kind"`
+	ProjectID int            `json:"project_id,omitempty"`
+	Project   webhookProject `json:"project,omitempty"`
+	Attr      webhookAttr    `json:"object_attributes,omitempty"`
+}
+
+func (w *webhook) entry(dec *json.Decoder, httpData *jsonapi.HTTP) (ret interface{}, err error) {
+	var param webhookPayload
+	if err := dec.Decode(&param); err != nil {
+		return nil, jsonapi.Error{http.StatusBadRequest, "Incorrect format"}
+	}
+
+	id := 0
+	name := ""
+	switch param.Kind {
+	case "push":
+		name = param.Project.Path
+		id = param.ProjectID
+	case "merge_request":
+		if param.Attr.State == "merged" {
+			name = param.Attr.Target.Path
+			id = param.Attr.TargetProjectID
+		}
+	}
+
+	if name != "" && id > 0 {
+		err = w.fetchProject(name, id)
+	}
+	if err != nil {
+		err = jsonapi.Error{http.StatusInternalServerError, err.Error()}
+	}
+
+	return
+}
+
+func (w *webhook) fetchProject(n string, id int) (err error) {
+	brs, err := w.client.ProjectBranches(strconv.Itoa(id))
+	if err != nil {
+		return
+	}
+
+	p := Project{
+		Name:     n,
+		Branches: make([]Branch, 0, len(brs)),
+	}
+
+	for _, br := range brs {
+		p.Branches = append(p.Branches, Branch{Name: br.Name})
+	}
+
+	return w.store.AddProjects([]Project{p})
+}
